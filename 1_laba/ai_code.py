9ec1e9edacfdb24e1c651e14279ad8a1637afbca
rbilokur2023 <rbilokur2023@itcollege.lviv.ua>
2025-12-15 02:57:35 +0200

    Add lab 1 files

---
+++ 1_laba/ai_code.py
@@ -0,0 +1,8 @@
+print("Hello, world!")
+
+name = input("Як тебе звати? ")
+print("Привіт,", name + "!")
+
+a = float(input("Введи перше число: "))
+b = float(input("Введи друге число: "))
+print("Сума:", a + b)
---
+++ 1_laba/code.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+name = "Roman"
+location = "Berlin"
+
+print(f"{name} start programming at {datetime.now()}. {location} is the best city!")
